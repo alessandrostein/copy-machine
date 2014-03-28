6295baa965c6e9936d56ff8246ac0741bff9c9cf
alessandrostein <ale182juve@msn.com>
2014-03-27 23:58:45 -0300

    Update

--- decorator/src/IRead.java
+++ decorator/src/IRead.java
@@ -1,3 +1,6 @@
+
+import java.awt.datatransfer.Clipboard;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +13,6 @@
  */
 public interface IRead {
     
-    void read();
+    Clipboard read();
     
 }
--- decorator/src/Read.java
+++ decorator/src/Read.java
@@ -1,9 +1,13 @@
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author alessandrostein
@@ -11,8 +15,13 @@
 public class Read implements IRead {
 
     @Override
-    public void read() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Clipboard read() {
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        String text = "Texto lido";
+        StringSelection selection = new StringSelection(text);
+        clipboard.setContents(selection, null);
+        
+        return clipboard;
     }
-    
+
 }
